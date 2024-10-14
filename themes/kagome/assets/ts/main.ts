74fc9e70e31c44f03219bc7501c03be83c977637
yuanxue <yuanxue@sensetime.com>
2024-10-14 17:52:21 +0800

    feat: 增加复制按钮

--- themes/kagome/assets/ts/main.ts
+++ themes/kagome/assets/ts/main.ts
@@ -207,3 +207,55 @@ declare var reloadPlantUML: (isDark: boolean) => void
     window.addEventListener('scroll', highlightTocLink);
   });
 })();
+
+(function () {
+  'use strict';
+
+  if (!document.queryCommandSupported('copy')) {
+    return;
+  }
+
+  function flashCopyMessage(el, msg) {
+    el.textContent = msg;
+    setTimeout(function () {
+      el.textContent = "Copy";
+    }, 1000);
+  }
+
+  function selectText(node) {
+    var selection = window.getSelection()!;
+    var range = document.createRange();
+    range.selectNodeContents(node);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    return selection;
+  }
+
+  function addCopyButton(containerEl) {
+    var copyBtn = document.createElement("button");
+    copyBtn.className = "highlight-copy-btn";
+    copyBtn.textContent = "Copy";
+
+    var codeEl = containerEl.querySelector('[data-lang]') || containerEl.querySelector('code');
+    copyBtn.addEventListener('click', function () {
+      try {
+        var selection = selectText(codeEl);
+        document.execCommand('copy');
+        selection.removeAllRanges();
+
+        flashCopyMessage(copyBtn, 'Copied!')
+      } catch (e) {
+        console && console.log(e);
+        flashCopyMessage(copyBtn, 'Failed :\'(')
+      }
+    });
+
+    containerEl.appendChild(copyBtn);
+  }
+
+  // Add copy button to code blocks
+  window.addEventListener('DOMContentLoaded', () => {
+    const highlightBlocks = document.getElementsByClassName('highlight');
+    Array.prototype.forEach.call(highlightBlocks, addCopyButton);
+  });
+})();
